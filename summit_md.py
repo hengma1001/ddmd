50dcb40615ca22d83950c8541ed18ce65e30266a
Heng Ma <38470774+hengma1001@users.noreply.github.com>
2020-03-05 16:25:31 -0600

    added radical profile

--- summit_md.py
+++ summit_md.py
@@ -24,6 +24,7 @@ if os.environ.get('RADICAL_ENTK_VERBOSE') is None:
 export RMQ_HOSTNAME=two.radical-project.org 
 export RMQ_PORT=33235 
 export RADICAL_PILOT_DBURL=mongodb://hyperrct:h1p3rrc7@two.radical-project.org:27017/hyperrct 
+export RADICAL_PROFILE=True
 export RADICAL_PILOT_PROFILE=True
 export RADICAL_ENTK_PROFILE=True
 '''
