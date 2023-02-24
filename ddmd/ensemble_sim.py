467da1772c1cbf13b03ec378ef094e8cea7a8973
Heng Ma <maheng1991@hotmail.com>
2023-02-24 21:09:41 +0000

    simplified top pdb pairing

--- ddmd/ensemble_sim.py
+++ ddmd/ensemble_sim.py
@@ -18,41 +18,17 @@ class ensemble_run(ddmd_run):
     def build_tasks(self):
         md_setup = self.ddmd_setup['md_setup']
         # correcting file path
-        input_files = ['pdb_file', 'top_file', 'checkpoint']
-        iter_conf = []
-        for input in input_files: 
-            if input in md_setup and md_setup[input]: 
-                if not os.path.isabs(md_setup[input]): 
-                    md_setup[input] = os.path.join(self.yml_dir, md_setup[input])
-                    logger.debug(f"updated entry{input} to {md_setup[input]}.")
-                if '*' in md_setup[input]: 
-                    md_setup[input] = glob.glob(md_setup[input])
-                    iter_conf.append(input)
-
+        md_setup['pdb_file'] = sorted(glob.glob(md_setup['pdb_file']))
         self.md_path = create_path(dir_type='md', time_stamp=False)
-        
+
         md_ymls= []
-        if iter_conf == []: 
-            md_yml = f"{self.md_path}/md.yml"
-            dict_to_yaml(md_setup, md_yml)
+        for i, pdb_file in enumerate(md_setup['pdb_file']): 
+            md_setup_copy = md_setup.copy()
+            md_setup_copy['pdb_file'] = pdb_file
+            md_setup_copy['top_file'] = pdb_file[:-3] + 'top'
+            md_yml = f"{self.md_path}/md_{i}.yml"
+            dict_to_yaml(md_setup_copy, md_yml)
             md_ymls.append(md_yml)
-        elif len(iter_conf) > 1: 
-            iter_conf_list = zip(*[md_setup[input] for input in iter_conf])
-            for i, conf in enumerate(iter_conf_list):
-                md_setup_copy = md_setup.copy()
-                for conf_name, conf_val in zip(iter_conf, conf): 
-                    md_setup_copy[conf_name] = conf_val
-                md_yml = f"{self.md_path}/md_{i}.yml"
-                dict_to_yaml(md_setup_copy, md_yml)
-                md_ymls.append(md_yml)
-        else: 
-            iter_conf_list = md_setup[iter_conf[0]]
-            for i, conf in enumerate(iter_conf_list):
-                md_setup_copy = md_setup.copy()
-                md_setup_copy[iter_conf[0]] = conf        
-                md_yml = f"{self.md_path}/md_{i}.yml"
-                dict_to_yaml(md_setup_copy, md_yml)
-                md_ymls.append(md_yml)
 
         return md_ymls
 
